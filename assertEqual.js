1843ee9d7125286efe583372c0752a138ec808bb
Hotae Kim <hotae.kim.94@gmail.com>
2021-05-31 16:18:53 +0000

    exported and test codes moved to different file

--- assertEqual.js
+++ assertEqual.js
@@ -7,8 +7,5 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual('Same', 'Same');
-assertEqual(1, 1);
-assertEqual(1, 2);
+//Export assertEqual
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢🟢🟢Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = (arr) => {
   /*
   return arr[0];
@@ -18,5 +10,4 @@ const head = (arr) => {
   return (arr && arr.length) ? arr[0] : undefined;
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
