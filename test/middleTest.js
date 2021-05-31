8f27905cbe982723df20b094eb223225a1dc7b02
Hotae Kim <hotae.kim.94@gmail.com>
2021-05-31 18:42:47 +0000

    deleted commented old test code

--- test/middleTest.js
+++ test/middleTest.js
@@ -2,12 +2,6 @@ const assert = require("chai").assert;
 const middle = require("../middle");
 
 //Test Code
-// assertArraysEqual(middle([1]), []); // => []
-// assertArraysEqual(middle([1, 2]), []); // => []
-// assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]); // => [2, 3]
-// assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); // => [3, 4]
 
 describe("#middle", () => {
   it("should return an empty array when array length of one is passed", () => {
