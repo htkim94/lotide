49b0d91bf1bec89b555a5e118b8f05c85816992e
Hotae Kim <hotae.kim.94@gmail.com>
2021-05-31 22:47:59 +0000

    flatten function refactored

--- flatten.js
+++ flatten.js
@@ -1,57 +1,9 @@
-//Function for checking if two arrays are equal
-const eqArrays = (arrOne, arrTwo) => {
-  if (arrOne.length !== arrTwo.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arrOne.length; i++) {
-      if (arrOne[i] !== arrTwo[i]) return false;
-    }
-    return true;
-  }
-};
-
-//Function for assertion message
-const assertArraysEqual = (arrA, arrB) => {
-  if (eqArrays(arrA, arrB)) {
-    console.log(`🟢🟢🟢Assertion Passed: ${arrA} === ${arrB}`);
-  } else {
-    console.log(`🔴🔴🔴Assertion Failed: ${arrA} !== ${arrB}`);
-  }
-};
-
-/* This is a recursion method.
-   If we want to flatten only one level,
-   to check if the recursion was gone through once
-   by using boolean.
-   However, if we want to fully flatten,
-   we need to remove the boolean part of the code,
-   so that recursion would repeat until it is fully flattened
-*/
-
-//Helper function for flatten
-const helperFlatten = (arr, resArr, bool) => {
-  for (let a of arr) {
-    if (Array.isArray(a) && bool === false) {
-      resArr = helperFlatten(a, resArr, true);
-    } else {
-      resArr.push(a);
-    }
-  }
-  return resArr;
-}
-
-//Main flatten function
+//flatten function using concat and recursion
 const flatten = (arr) => {
-  let res = [];
-  return helperFlatten(arr, res, false);
-}
-
-//flatten function without helper using concat
-const fn = (arr) => {
   let res = [];
   for (const a of arr) {
     if (Array.isArray(a)) {
-      res = res.concat(fn(a));
+      res = res.concat(flatten(a));
     } else {
       res.push(a);
     }
@@ -59,7 +11,4 @@ const fn = (arr) => {
   return res;
 };
 
-console.log(flatten([1, 2, [3, 4], 5, [6, 7, [8, 9]]]));
-console.log(fn([1, 2, [3, 4], 5, [6, 7, [8, 9]]]));
-console.log(flatten([1, [2, [3, [4]], 5]]));
-console.log(fn([1, [2, [3, [4]], 5]]));
+module.exports = flatten;
